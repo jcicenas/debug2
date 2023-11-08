3ec71b310376fa2e517e5a2508c988c3cc9ec2fd
John Cicenas <jcicenas2@gmail.com>
2023-11-08 09:24:23 -0500

    yes

---
+++ Debug.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Debug {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    String s;
+    String current;
+    int uniqueCount = 0;
+    int count;
+        System.out.println("Please enter a word or phrase");
+        s = scan.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+            current = s.substring(i, i+1);
+            if ( i == 0 || !s.substring(0, i).contains(current)){
+                uniqueCount++;
+                count = 1;
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (current.equals(s.substring(j, j+1))) {
+                        count++;
+                    }
+                }
+                System.out.println(current + " occurs " + count + " time(s).");
+            }
+        }
+        System.out.println("There are " + uniqueCount + " unique characters");
+        scan.close();
+    }
+}
